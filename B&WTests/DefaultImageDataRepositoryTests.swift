fbf906548a9d3766706e6b9bfec097a7773c220f
tzc1234 <peopli9997@gmail.com>
2024-04-04 12:49:04 +0100

    DefaultImageDataRepository.init does not notify dataTransferService

---
+++ B&WTests/DefaultImageDataRepositoryTests.swift
@@ -0,0 +1,59 @@
+//
+//  DefaultImageDataRepositoryTests.swift
+//  B&WTests
+//
+//  Created by Tsz-Lung on 04/04/2024.
+//  Copyright © 2024 Artemis Simple Solutions Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import B_W
+
+protocol ImageDataEndpoints {
+    
+}
+
+struct ImageDataRepositoryEndpoints: ImageDataEndpoints {
+    
+}
+
+class DefaultImageDataRepository {
+    private let endpoints: ImageDataEndpoints
+    private let dataTransferService: DataTransferService
+    
+    init(endpoints: ImageDataEndpoints, dataTransferService: DataTransferService) {
+        self.endpoints = endpoints
+        self.dataTransferService = dataTransferService
+    }
+}
+
+final class DefaultImageDataRepositoryTests: XCTestCase {
+    func test_init_doesNotNotifyDataTransferService() {
+        let (_, service) = makeSUT()
+        
+        XCTAssertEqual(service.requestCallCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> (sut: DefaultImageDataRepository, service: DataTransferServiceSpy) {
+        let endpoints = ImageDataRepositoryEndpoints()
+        let service = DataTransferServiceSpy()
+        let sut = DefaultImageDataRepository(endpoints: endpoints, dataTransferService: service)
+        return (sut, service)
+    }
+    
+    private class DataTransferServiceSpy: DataTransferService {
+        private(set) var requestCallCount = 0
+        
+        struct Cancellable: NetworkCancellable {
+            func cancel() {}
+        }
+        
+        func request<T: Decodable, E: ResponseRequestable>(with endpoint: E,
+                                                           completion: @escaping CompletionHandler<T>) -> NetworkCancellable? where E.Response == T {
+            requestCallCount += 1
+            return Cancellable()
+        }
+    }
+}
