d6a3f1835db865c55c55613b656659c11ae72d86
tzc1234 <peopli9997@gmail.com>
2024-04-04 21:01:50 +0100

    DefaultProductsListViewModel.init does not notify its collaborators

--- B&WTests/ProductsListViewModelTests.swift
+++ B&WTests/ProductsListViewModelTests.swift
@@ -1,5 +1,39 @@
 import XCTest
 @testable import B_W
 
-class ProductsListViewModelTests: XCTestCase {
+final class ProductsListViewModelTests: XCTestCase {
+    func test_init_doesNotNotifyCollaborators() {
+        let (_, getProducts, loadImage) = makeSUT()
+        
+        XCTAssertEqual(getProducts.executeCallCount, 0)
+        XCTAssertEqual(loadImage.loadCallCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(showProductDetails: @escaping (Product) -> Void = { _ in },
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: ProductsListViewModel, getProducts: GetProductsUseCaseSpy, loadImage: LoadImageDataUseCaseSpy) {
+        let actions = ProductsListViewModelActions(showProductDetails: showProductDetails)
+        let getProducts = GetProductsUseCaseSpy()
+        let loadImage = LoadImageDataUseCaseSpy()
+        let sut = DefaultProductsListViewModel(
+            useCase: getProducts,
+            actions: actions, 
+            loadImageDataUseCase: loadImage
+        )
+        trackForMemoryLeaks(getProducts, file: file, line: line)
+        trackForMemoryLeaks(loadImage, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, getProducts, loadImage)
+    }
+    
+    private final class GetProductsUseCaseSpy: GetProductsUseCase {
+        private(set) var executeCallCount = 0
+        
+        func execute(requestValue: GetProductsUseCaseRequestValue, completion: @escaping (Result<Products, any Error>) -> Void) -> Cancellable? {
+            executeCallCount += 1
+            return nil
+        }
+    }
 }
