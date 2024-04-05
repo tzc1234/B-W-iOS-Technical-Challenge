98ddcce99e301def7e7ab646cac750eea5850c3f
tzc1234 <peopli9997@gmail.com>
2024-04-05 23:01:56 +0100

    Add comment of the purpose of DefaultLoadImageDataUseCase

--- B&W/Domain/UseCases/LoadImageDataUseCase.swift
+++ B&W/Domain/UseCases/LoadImageDataUseCase.swift
@@ -13,6 +13,18 @@ protocol LoadImageDataUseCase {
     func load(for url: URL, completion: @escaping Completion) -> Cancellable
 }
 
+// DefaultLoadImageDataUseCase will encapsulate the business logic/rules, knowing "what to do".
+// The business logic of this use case
+//  - happy path: load image data by an URL
+//  - sad path: delivers failed/noData error
+
+// In order to emphasise this business logic, this component will delegate "how to do" to someone.
+// Asking for an image data from its collaborator through the `ImageDataRepository` protocol, the `URL` param,
+// and finally get the `Data` back.
+// This whole loading process embodies the business logic: load image data by an URL. Similarly the sad path.
+
+// Also in order to emphasise this business logic, avoiding directly load image data from low-level components/abstractions.
+// Let those low-level components/abstractions utilise by the collaborator (ImageDataRepository).
 final class DefaultLoadImageDataUseCase: LoadImageDataUseCase {
     private let repository: ImageDataRepository
     
