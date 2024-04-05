f561893d2481dd6c936e20659be53649ee4086d2
tzc1234 <peopli9997@gmail.com>
2024-04-05 07:15:04 +0100

    Tidy up AppDelegate

--- B&W/Application/AppDelegate.swift
+++ B&W/Application/AppDelegate.swift
@@ -2,29 +2,28 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    let appDIContainer = AppDependenciesContainer()
-    var appFlowCoordinator: AppFlowCoordinator?
+    private let appDIContainer = AppDependenciesContainer()
+    private var appFlowCoordinator: AppFlowCoordinator?
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+    func application(_ application: UIApplication, 
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
 
         let navigationController = UINavigationController()
-
         let tabBarController = UITabBarController()
 
         window?.rootViewController = tabBarController
         
-        appFlowCoordinator = AppFlowCoordinator(tabBarController: tabBarController,
-                                                navigationController: navigationController,
-                                                appDependencies: appDIContainer)
+        appFlowCoordinator = AppFlowCoordinator(
+            tabBarController: tabBarController,
+            navigationController: navigationController,
+            appDependencies: appDIContainer
+        )
         appFlowCoordinator?.start()
 
         window?.makeKeyAndVisible()
 
         return true
     }
-
 }
