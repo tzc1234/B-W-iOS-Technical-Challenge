552dcfc458f54e64eb85755ef68f69bebc138c8d
tzc1234 <peopli9997@gmail.com>
2024-04-07 08:49:34 +0100

    Choose a more "business" naming for url, it should be the same as the imagePath in Product entity

--- B&W/Domain/UseCases/LoadImageDataUseCase.swift
+++ B&W/Domain/UseCases/LoadImageDataUseCase.swift
@@ -15,16 +15,16 @@ protocol LoadImageDataUseCase {
 
 // DefaultLoadImageDataUseCase will encapsulate the business logic/rules, knowing "what to do".
 // The business logic of this use case
-//  - happy path: load image data by an URL
-//  - sad path: delivers failed/noData error
+//  - happy path: load image data by an image path
+//  - sad path: delivers failed/no data error
 
-// In order to emphasise this business logic, this component will delegate "how to do" to someone.
-// Asking for an image data from its collaborator through the `ImageDataRepository` protocol, the `URL` param,
-// and finally get the `Data` back.
-// This whole loading process embodies the business logic: load image data by an URL. Similarly the sad path.
+// In order to emphasise this business logic, this use case component will delegate "how to do" to someone.
+// Asking for an image data from its collaborator through the `ImageDataRepository` protocol and the `URL` param,
+// finally get the `Data` back.
+// This whole loading process embodies the business logic: load image data by an image path. Similarly the sad path.
 
 // Also in order to emphasise this business logic, avoiding directly load image data from low-level components/abstractions.
-// Let those low-level components/abstractions utilise by the collaborator (ImageDataRepository).
+// Let those low-level components/abstractions be utilised by the collaborator (ImageDataRepository).
 final class DefaultLoadImageDataUseCase: LoadImageDataUseCase {
     private let repository: ImageDataRepository
     
@@ -37,8 +37,8 @@ final class DefaultLoadImageDataUseCase: LoadImageDataUseCase {
         case noData
     }
     
-    func load(for url: URL, completion: @escaping Completion) -> Cancellable {
-        repository.fetchImageData(for: url) { result in
+    func load(for imagePath: URL, completion: @escaping Completion) -> Cancellable {
+        repository.fetchImageData(for: imagePath) { result in
             switch result {
             case let .success(data):
                 guard let data else {
