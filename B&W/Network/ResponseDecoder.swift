bda1c32851baf8a9707e002b4fb627c70555fd27
tzc1234 <peopli9997@gmail.com>
2024-04-08 18:28:18 +0100

    Remove unused RawDataResponseDecoder

--- B&W/Network/ResponseDecoder.swift
+++ B&W/Network/ResponseDecoder.swift
@@ -22,22 +22,3 @@ public final class JSONResponseDecoder: ResponseDecoder {
         return try jsonDecoder.decode(T.self, from: data)
     }
 }
-
-// MARK: - RawDataResponseDecoder
-
-public final class RawDataResponseDecoder: ResponseDecoder {
-    public init() { }
-
-    enum CodingKeys: String, CodingKey {
-        case `default` = ""
-    }
-    
-    public func decode<T: Decodable>(_ data: Data) throws -> T {
-        if T.self is Data.Type, let data = data as? T {
-            return data
-        } else {
-            let context = DecodingError.Context(codingPath: [CodingKeys.default], debugDescription: "Expected Data type")
-            throw Swift.DecodingError.typeMismatch(T.self, context)
-        }
-    }
-}
