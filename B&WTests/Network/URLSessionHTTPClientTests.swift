a99f4917e6f5836155418e3b1c2ba22fc54afef3
tzc1234 <peopli9997@gmail.com>
2024-04-28 14:43:19 +0100

    URLSessionHTTPClient.request fails on request error

--- B&WTests/Network/URLSessionHTTPClientTests.swift
+++ B&WTests/Network/URLSessionHTTPClientTests.swift
@@ -15,9 +15,11 @@ final class URLSessionHTTPClient {
         self.session = session
     }
     
-    func request(_ request: URLRequest) {
+    func request(_ request: URLRequest, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
         let task = session.dataTask(with: request) { data, response, error in
-            
+            if let error {
+                completion(.failure(error))
+            }
         }
         task.resume()
     }
@@ -35,7 +37,24 @@ final class URLSessionHTTPClientTests: XCTestCase {
             XCTAssertEqual(request, expectedRequest)
             exp.fulfill()
         }
-        sut.request(expectedRequest)
+        sut.request(expectedRequest) { _ in }
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func test_request_failsOnRequestError() {
+        let sut = makeSUT()
+        URLProtocolStub.stub(data: nil, response: nil, error: anyNSError())
+        
+        let exp = expectation(description: "Wait for completion")
+        sut.request(URLRequest(url: anyURL())) { result in
+            switch result {
+            case .failure:
+                break
+            default:
+                XCTFail("Should be a failure")
+            }
+            exp.fulfill()
+        }
         wait(for: [exp], timeout: 1)
     }
     
@@ -51,7 +70,24 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
     
     private final class URLProtocolStub: URLProtocol {
+        private struct Stub {
+            let data: Data?
+            let response: HTTPURLResponse?
+            let error: Error?
+        }
+        
         static var observer: ((URLRequest) -> Void)?
+        
+        private static let queue = DispatchQueue(label: "URLProtocolStub.queue")
+        private static var _stub: Stub?
+        private static var stub: Stub? {
+            get { queue.sync { _stub } }
+            set { queue.sync { _stub = newValue } }
+        }
+        
+        static func stub(data: Data?, response: HTTPURLResponse?, error: Error?) {
+            stub = Stub(data: data, response: response, error: error)
+        }
         
         override class func canInit(with request: URLRequest) -> Bool {
             true
@@ -62,7 +98,21 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override func startLoading() {
-            client?.urlProtocolDidFinishLoading(self)
+            let stub = Self.stub
+            
+            if let data = stub?.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = stub?.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+            
+            if let error = stub?.error {
+                client?.urlProtocol(self, didFailWithError: error)
+            } else {
+                client?.urlProtocolDidFinishLoading(self)
+            }
             
             Self.observer?(request)
         }
